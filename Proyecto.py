57e46067f75689684b7c4eadcbbec090c980bfac
JorgeM28 <jorge2224764@correo.uis.edu.co>
2022-11-26 09:50:17 -0500

    Se AGREGO UNA VENTANA SECUNDARIA QUE MUETRA LOS DATOS INGRESADOS Y LOS BOTONES DE SALIDA

--- Proyecto.py
+++ Proyecto.py
@@ -1,4 +1,9 @@
 from tkinter import *
+from tkinter import messagebox
+
+def salir():
+    ventana.destroy()
+
 
 def send_data():
     username_data =username.get()
@@ -6,21 +11,26 @@ def send_data():
     tipodevehículo_data =tipodevehículo.get()
     placa_data =str(placa.get())
     tiempo_data =tiempo.get()
-    print(username_data, "\t", código_data, "\t", tipodevehículo_data, "\t", placa_data, "\t", tiempo_data)
+    print("NOMBRE: ",username_data, "\t","CODIGO",código_data, "\t","TIPO DE VEHICILO" ,tipodevehículo_data, "\t","PLACA ", placa_data, "\t","LUGAR DE ESTACIONAMIENTO", tiempo_data)
 
     newfile=open("Registros.txt", "a")
+    newfile.write("NOMBRE: ")
     newfile.write(username_data)
     newfile.write("\t")
+    newfile.write("CODIGO: ")
     newfile.write(código_data)
     newfile.write("\t")
+    newfile.write("TIPO DE VEHICULO: ")
     newfile.write(tipodevehículo_data)
     newfile.write("\t")
+    newfile.write("PLACA: ")
     newfile.write(placa_data)
     newfile.write("\t")
+    newfile.write("LUGAR DE ESTACIONAMIENTO: ")
     newfile.write(tiempo_data)
     newfile.write("\n")
     newfile.close()
-    print("Nuevo usuario registrado. NOMBRE: {} | TIEMPO: {} ".format(username_data, tiempo_data))
+    print("Nuevo usuario registrado. NOMBRE: {} | LUGAR DE ESTACIONAMIENTO: {} ".format(username_data, tiempo_data))
 
     username_entry.delete(0, END)
     código_entry.delete(0, END)
@@ -38,21 +48,43 @@ lb_logo.place(x=480,y=150)
 
 ventana.resizable(False,False)
 ventana.config(background="#E8EAEC")
-lbl =Label(ventana,text="Hola bienvendido a la UIS por favor ingresa tus datos y los de tu vehículo: ", font=("Cambria", 13), bg="#56cd63", fg="white", width="550", height="2")
+lbl =Label(ventana,text="Bienvendido a la UIS por favor ingresa tus datos y los de tu vehículo: ", font=("Cambria", 13), bg="#56cd63", fg="white", width="550", height="2")
 lbl.pack()
 ventana.config(bg="green")
 
 
 
+def ventana_sec1():
+    ventana_sec = Toplevel()
+    ventana_sec.title("VEHICULOS EN EL PARQUEADERO")
+    ventana_sec.geometry("950x500")
+    ventana_sec.config(bg="green")
+    frame_resultados = Frame(ventana_sec)
+    frame_resultados.config(bg="white", width=930, height=480)
+    frame_resultados.place(x=10, y = 10)
+    t_resultados = Text(frame_resultados)
+    t_resultados.config(bg="green", fg="black", font=("Arial",10))
+    t_resultados.place(x=10,y=10, width=910, height= 400)
+    def exit():
+        ventana_sec.destroy()
+    salir_btn =Button(ventana_sec, text="Salir de la ventana", command=exit, width="30", height="2")
+    salir_btn.place(x=360, y=435)
+    with open("Registros.txt") as archivo:
+        for linea in archivo:
+            t_resultados.insert(INSERT, linea)
+
+    
+    
+    
 username_label = Label(text="NOMBRE COMPLETO", bg="#FFEEDD")
 username_label.place(x=22, y=70)
 código_label = Label(text="CÓDIGO", bg="#FFEEDD")
 código_label.place(x=22, y=130)
-tipodevehículo_label = Label(text="TIPO DE VEHÍCULO", bg="#FFEEDD")
+tipodevehículo_label = Label(text="TIPO DE VEHÍCULO, ¿CARRO Ó MOTO?", bg="#FFEEDD")
 tipodevehículo_label.place(x=22, y=190)
 placa_label = Label(text="PLACA", bg="#FFEEDD")
 placa_label.place(x=22, y=250)
-tiempo_label = Label(text="TIEMPO DE ESTADÍA EN EL PARQUEADERO", bg="#FFEEDD")
+tiempo_label = Label(text="LUGAR DE ESTACIONAMIENTO", bg="#FFEEDD")
 tiempo_label.place(x=22, y=310)
 
 username = StringVar()
@@ -75,5 +107,13 @@ tiempo_entry.place(x=22, y=340)
 
 submit_btn =Button(ventana, text="Cargar datos", command=send_data, width="30", height="2")
 submit_btn.place(x=22, y=390)
+
+mostrar_btn =Button(ventana, text="Mostar datos", command=ventana_sec1, width="30", height="2")
+mostrar_btn.place(x=250, y=390)
+
+salir_btn =Button(ventana, text="Salir de la app", command=salir, width="30", height="2")
+salir_btn.place(x=478, y=390)
+
+
 
 ventana.mainloop()
